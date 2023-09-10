e2a4f00c8a804ff02e72186ee2f40275d2db0e75
Nikita Filippov <nikita.filippov.aleks@mail.ru>
2023-09-10 17:53:33 +0300

    Bell animtaions

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ const tl = gsap.timeline({
   defaults: { duration: 0.35, ease: 'Power2.easeOut' }
 });
 
+const houseTl = gsap.timeline({
+  defaults: {
+    duration: 1
+  }
+});
+
 const home = document.querySelector('.home');
+const notification = document.querySelector('.notification');
 
 gsap.set('.feather', { scale: 0, transformOrigin: 'center' })
 home.addEventListener('click', () => {
   gsap.fromTo('.birdhouse-svg', { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 })
-  gsap.fromTo('#Feather_1', { x: 0, opacity: 1, rotation: 0 }, { x: -20, scale: 1.5, rotation: '-90deg', duration: 1, opacity: 0, ease: "slow(0.1, 0.7, true)" })
+  gsap.fromTo('#Roof', { y: 0, x: 0, rotation: 0 }, { y: -3, x: 1, rotation: '4deg', yoyo: true, repeat: 1 })
+  houseTl.fromTo('#Feather_1', { x: 0, rotation: 0 }, { x: -20, scale: 1.5, rotation: '-90deg' })
+  houseTl.fromTo('#Feather_2', { x: 0, y: 0, rotation: 0 }, { x: 20, y: -10, scale: 1.5, rotation: '90deg' }, '<')
+  houseTl.fromTo('#Feather_3', { x: 0, y: 0, rotation: 0 }, { x: 10, y: 10, scale: 1.5, rotation: '90deg' }, '<')
+  houseTl.fromTo('#Feather_1', { opacity: 1 }, { opacity: 0 }, '<30%')
+  houseTl.fromTo('#Feather_2', { opacity: 1 }, { opacity: 0 }, '<')
+  houseTl.fromTo('#Feather_3', { opacity: 1 }, { opacity: 0 }, '<')
+})
+
+gsap.set('#Bell', { transformOrigin: 'top center' } )
+gsap.set('#Tongue', { transformOrigin: 'top center' } )
+gsap.set('#Sound', { opacity: 0, transformOrigin: 'bottom' } )
+notification.addEventListener('click', () => {
+  gsap.fromTo('#Bell', { rotation: -5 }, { rotation: 0, duration: 2, ease: "elastic.out(5, 0.2)" })
+  gsap.fromTo('#Tongue', { rotation: -5, x: 0.5 }, { rotation: 0, x: 0, duration: 2, ease: "elastic.out(5, 0.2)" })
+  gsap.fromTo('#Sound', { opacity: 1, scale: 0.6 }, { opacity: 0, scale: 1.3, duration: 1 })
 })
